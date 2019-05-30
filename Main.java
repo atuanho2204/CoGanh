40cbe1a361e779a734769e27f37a15f42ac69543
Anh Tuan Ho <47576984+itto22041999@users.noreply.github.com>
2019-05-30 13:03:55 -0400

    updated move

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
 	public static void main(String[] args) {
 		Board board = new Board();
 		System.out.println((new Move(board, new Position(0, 1), new Position(0, 0))).isValid());
-		System.out.println((new Move(board, new Position(0, 1), new Position(0, 1))).isValid());
-		System.out.println((new Move(board, new Position(0, 1), new Position(1, 1))).isValid());
-		System.out.println((new Move(board, new Position(0, 1), new Position(2, 2))).isValid());
+		System.out.println((new Move(board, new Position(1, 1), new Position(3, 3))).isValid());
+		System.out.println((new Move(board, new Position(0, 1), new Position(1, 2))).isValid());
+		System.out.println((new Move(board, new Position(1, 2), new Position(2, 1))).isValid());
 	}
 }	
