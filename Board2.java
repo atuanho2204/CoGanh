972c7a0aa62436299de6eefc30ae69941da2fcc8
cuom1999 <lephuocdinh99@gmail.com>
2019-05-30 15:50:20 +0700

    fix Board

--- Board2.java
+++ Board2.java
@@ -3,9 +3,9 @@ public class Board {
 	private final NUM_COLS = 5;
 
 	public void print() {
-		for (int i = NUM_ROWS - 1; i >= 0; i--) {
-			for (int j = 0; j < NUM_COLS; j++) {
-				System.out.print(grid[i][j]);
+		for (int y = 0; y < NUM_COLS; y--) {
+			for (int x = 0; x < NUM_ROWS; x++) {
+				System.out.print(grid[x][y]);
 			}
 			System.out.println();
 		} 
