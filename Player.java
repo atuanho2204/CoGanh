57f4bd65192ac736e147535b7772d39e3c420619
cuom1999 <lephuocdinh99@gmail.com>
2019-06-03 11:12:34 +0700

    add Player.java

---
+++ Player.java
@@ -0,0 +1,23 @@
+public class Player {
+	private String name;
+
+	public void setName() {
+
+	}
+
+	public void getName() {
+
+	}
+
+	public Piece getPiece(int x, int y) {
+
+	}
+
+	public Piece[][] getBoard(Board board) {
+
+	}
+
+	public void makeMove(Move oldMove, Move newMove) {
+
+	}
+}
